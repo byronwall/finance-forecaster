582c8e8507a98f84bd7a3d6ee341f9bd79b3d050
Byron Wall <byron@byroni.us>
2017-07-11 22:01:28 -0600

    Move to Components
    Move the Components into their own folder.

--- src/Components/RecurringInput.tsx
+++ src/Components/RecurringInput.tsx
@@ -1,13 +1,22 @@
 import * as React from "react";
 import { Component } from "react";
 import { FormGroup, Glyphicon, FormControl } from "react-bootstrap";
+import { CashAccount } from "../Models/Account";
 
-export default class RecurringInput extends Component<any, any> {
+interface RecurringProp {
+    input: CashAccount;
+    id: number;
+    handleChange: (obj: {}) => void;
+}
 
-    handleChange(e: any, key: string) {
+export default class RecurringInput extends Component<RecurringProp, {}> {
+
+    handleChange(e: React.FormEvent<any>, key: string) {
         console.log("recurring input", e, key);
 
-        let amount = Number.parseFloat(e.target.value);
+        let target = e.target as HTMLInputElement;
+
+        let amount = Number.parseFloat(target.value);
         amount = Number.isNaN(amount) ? 0 : amount;
 
         this.props.handleChange({
@@ -26,7 +35,7 @@ export default class RecurringInput extends Component<any, any> {
                     <FormGroup>
                         <FormControl
                             type="text"
-                            value={inputData.amount}
+                            value={inputData.startAmount}
                             placeholder="Starting amount"
                             onChange={(e) => this.handleChange(e, "amount")}
                         />
@@ -34,24 +43,24 @@ export default class RecurringInput extends Component<any, any> {
                 </td>
 
                 <td>
-                    <FormGroup>
+                    {/* <FormGroup>
                         <FormControl
                             type="text"
                             value={inputData.frequency}
                             placeholder="Rate"
                             onChange={(e) => this.handleChange(e, "frequency")}
                         />
-                    </FormGroup>
+                    </FormGroup> */}
                 </td>
                 <td>
-                    <FormGroup>
+                    {/* <FormGroup>
                         <FormControl
                             type="text"
                             value={inputData.delay}
                             placeholder="Delay"
                             onChange={(e) => this.handleChange(e, "delay")}
                         />
-                    </FormGroup>
+                    </FormGroup> */}
                 </td>
 
                 <td>
